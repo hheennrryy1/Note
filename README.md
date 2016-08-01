e8bc0a56cf3c9fc3f6edce632a1d91d1200cd8a9
hheennrryy1 <zenghenghao@foxmail.com>
2016-08-01 17:36:18 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-#笔记本系统
+##Note 笔记本系统
+###Framework and Tools 框架和工具
+* Java&IDE: JDK8 Eclipse
+* Backend: SpringMVC4.2.5 Spring4.2.5 MyBatis3.4.0 (Configured by annotation)
+* Frontend: Bootstrap Bootstrap-validator jQuery jshashes Flat-UI Font-Awesome 
+* Database: MySQL5.6
+* Web Server: Tomcat 8
+* Build Tool: Maven
+* Other: Druid(database connection pool) JUnit Log4j Jackson
+
+###System Features 系统特性
+* Integration(整合) of Spring core, Spring MVC, and MyBatis
+* Management of notebooks and notes through CRUD interface 管理笔记本、笔记，实现增删改查
+* Management of user 用户管理包括登录(log in)、注册(sign up)、修改密码(change password)、登出(Log out)
+
+###Demo 截图
+Log in 登录     
+![登录界面](images/Demo1.png)    
+Management of notebooks 笔记本管理    
+![笔记本管理](images/Demo2.png)    
+Create note 创建笔记    
+![创建笔记](images/Demo3.png)    
+
---
+++ SQL/note.sql
@@ -0,0 +1,49 @@
+﻿SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `note`
+-- ----------------------------
+DROP TABLE IF EXISTS `note`;
+CREATE TABLE `note` (
+  `nid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(15) DEFAULT NULL,
+  `content` text,
+  `createtime` datetime DEFAULT NULL,
+  `updatetime` datetime DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `notebook_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`nid`),
+  KEY `notebook_id` (`notebook_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `note_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `notebook`
+-- ----------------------------
+DROP TABLE IF EXISTS `notebook`;
+CREATE TABLE `notebook` (
+  `nbid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`nbid`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `notebook_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL,
+  `salt` varchar(24) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
