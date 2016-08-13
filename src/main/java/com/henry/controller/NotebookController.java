ec2403d2d41de5bea1cff0546323eebb0518ebff
hheennrryy1 <zenghenghao@foxmail.com>
2016-08-13 15:09:16 +0800

    fix

--- src/main/java/com/henry/controller/NotebookController.java
+++ src/main/java/com/henry/controller/NotebookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,8 @@ import com.henry.service.NotebookService;
 @Controller
 @RequestMapping("/notebook")
 public class NotebookController {
+	
+	Logger logger = Logger.getLogger(NotebookController.class);
 	
 	@Autowired
 	private NotebookService notebookService;
